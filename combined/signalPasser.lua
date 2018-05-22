1d5755be85ecd3e87ce084a5610454dedddf4252
Erik Roelofs <erik.roelofs@procurios.eu>
2018-05-22 09:04:30 +0200

    Show wire directions; fix settings wire directions.

--- combined/signalPasser.lua
+++ combined/signalPasser.lua
@@ -2,10 +2,26 @@ return function(objectFactory, id, inputDirections, triggerDirections)
   local block = objectFactory(id)
     :thatIsSolid()
     :thatCanBePushed()
-    :thatIsAnInput(directions)    
-    :thatIsATrigger(directions)
+    :thatIsAnInput(inputDirections)    
+    :thatIsATrigger(triggerDirections)
     :thatCanBeDrawn(function(self) 
-      love.graphics.print("wire", 5, 20) 
+      --love.graphics.print("wire", 5, 20) 
+      love.graphics.setColor(1,0,0)
+      love.graphics.circle("fill", CONST.TILE_WIDTH / 2, CONST.TILE_HEIGHT / 2, 8)
+      if self.inputDirections.up or self.triggerDirections.up then
+          love.graphics.rectangle("fill", CONST.TILE_WIDTH / 2 - 2, CONST.TILE_HEIGHT / 2 - 2, 4, CONST.TILE_HEIGHT / 2)
+      end
+      if self.inputDirections.down or self.triggerDirections.down then
+          love.graphics.rectangle("fill", CONST.TILE_WIDTH / 2 - 2, CONST.TILE_HEIGHT / 2 + 2, 4, -CONST.TILE_HEIGHT / 2)
+      end
+      if self.inputDirections.left or self.triggerDirections.left then
+          love.graphics.rectangle("fill", CONST.TILE_WIDTH / 2 - 2, CONST.TILE_HEIGHT / 2 - 2,  -CONST.TILE_WIDTH / 2 , 4)
+      end
+      if self.inputDirections.right or self.triggerDirections.right then
+          love.graphics.rectangle("fill", CONST.TILE_WIDTH / 2 - 2, CONST.TILE_HEIGHT / 2 - 2,  CONST.TILE_WIDTH / 2 , 4)
+      end
+        
+      love.graphics.setColor(1,1,1)
       self:drawActiveMark()
     end)
     :go()
--- levels/1.lua
+++ levels/1.lua
@@ -11,8 +11,8 @@ return function(dispatcher, library)
   room:placeObject(1, 0, library.input.key(id(), "s"))
 
   room:placeObject(0, 3, library.trigger.pusher(id(), nil, { right = true }))
-  room:placeObject(1, 3, library.combined.wire(id()))
-  room:placeObject(2, 3, library.combined.wire(id()))
+  room:placeObject(1, 3, library.combined.wire(id(), {left = true, right = true},{left = true, right = true}))
+  room:placeObject(2, 3, library.combined.wire(id(), {left = true, right = true},{left = true, right = true}))
   room:placeObject(3, 3, library.input.key(id(), "a", { down = true }))
 
   local doorId = id()
